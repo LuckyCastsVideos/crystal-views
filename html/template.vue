4858b5afe1eba5e8c3708895aef895b61347d583
Stephen Dolan <dolan.stephen1@gmail.com>
2020-08-24 16:59:10 -0400

    Add vue

--- html/template.vue
+++ html/template.vue
@@ -1,8 +1,33 @@
 <template>
-  <li>
-    {{ todo.text }}
-    <button @click="$emit('remove', todo.id)">
-      X
-    </button>
-  </li>
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Content</th>
+        <th colspan="3"></th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="book in books">
+        <td>{{ book.title }}</td>
+        <td>{{ book.content }}</td>
+        <td><router-link :to="{ path: 'book', params: { bookId: book.id }">Show</router-link></td>
+        <td><router-link :to="{ path: 'bookEdit', params: { bookId: book.id }">Edit</router-link></td>
+        <td><router-link :to="{ path: 'bookDelete', params: { bookId: book.id }">Delete</router-link></td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      books: [
+        ...some books
+      ]
+    }
+  },
+}
+</script>
